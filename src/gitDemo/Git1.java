cfe10bacaea32c4b46701d218ef9c3ed1e0bc73f
tomato <Tomato@163.com>
2024-04-06 16:37:41 +0800

    third commit

--- src/gitDemo/Git1.java
+++ src/gitDemo/Git1.java
@@ -4,6 +4,7 @@ public class Git1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Use Git in IDEA");
+        System.out.println("Hello World!!!!");
     }
 }
 
