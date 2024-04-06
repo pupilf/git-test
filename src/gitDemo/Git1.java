364ee5dbaa9084d830afa2a4ed98dff7228487b2
tomato <Tomato@163.com>
2024-04-06 16:35:35 +0800

    second commit

--- src/gitDemo/Git1.java
+++ src/gitDemo/Git1.java
@@ -3,6 +3,7 @@ package gitDemo;
 public class Git1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Use Git in IDEA");
     }
 }
 
